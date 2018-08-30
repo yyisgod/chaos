29da0b452dffab88b01cc8e0d14c1e1f4d919502
photonCarry <yyisgod@yeah.net>
2018-08-30 16:35:32 +0800

    cpp init

---
+++ cpp/linux/deamon.cpp
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+using namespace std;
+
+bool daemonize()
+{
+	pid_t pid = fork();
+	if (pid < 0)
+	{
+		return false;
+	}
+	else if (pid > 0)
+	{
+		exit(0);
+	}
+
+	umask(0);
+
+	pid_t sid = setsid();
+	if (sid < 0)
+	{
+		return false;
+	}
+
+	if ((chdir("/")) < 0)
+	{
+		return false;
+	}
+
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	open("/dev/null", O_RDONLY);
+	open("/dev/null", O_RDWR);
+	open("/dev/null", O_RDWR);
+	return true;
+}
+
+int main()
+{
+	if (daemonize()) {
+		while (true) {
+			sleep(1000);
+		}
+	}
+	// daemon(0, 0);
+}
